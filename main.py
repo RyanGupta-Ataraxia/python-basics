c4954104b485a83e08608b02ed2407034f24d323
IamRyanGupta <ryangupta680@gmail.com>
2025-10-24 09:24:32 +0530

    Integrate math functions into the Python toolkit
    Added math functions to the toolkit and updated user prompts.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from functions.string_functions import *
 from functions.list_functions import *
 from functions.dictionary_functions import *
+from functions.math_functions import*
 
 print("Welcome to Ryan Gupta’s Python Basics Toolkit!")
 print("Choose a category:")
 print("1. String functions")
 print("2. List functions")
 print("3. Dictionary functions")
+print("4. Math functions")
 
-choice = input("Enter 1, 2, or 3: ")
+choice = input("Enter 1, 2,3 or 4: ")
 
 if choice == "1":
     text = input("Enter a string: ")
@@ -49,6 +51,26 @@ elif choice == "3":
     d1 = {'a': 1, 'b': 2}
     d2 = {'b': 3, 'c': 4}
     print("Merging example dictionaries:", merge_dicts(d1, d2))
+
+elif choice == "4":
+    num1 = input("Enter first number")
+    num2 = input("Enter secondnumber")
+    print(" 1. Add numbers")
+    print(" 2. Subtract numbers")
+    print(" 3. Multiply numbers")
+    print(" 4. Divide numbers")
+    print(" 5. Factorial of numbers")
+    func = input ("Enter your choice: ")
+    if func == "1":
+        print (add(num1, num2)
+    elif func == "2":
+        print(subtract(num1, num2))
+    elif func == "3":
+        print(multiply(num1, num2))
+    elif func == "4":
+        print(divide(num1, num2))
+    elif func == "5":
+        print(factorial(num1, num2))
 
 else:
     print("Invalid choice. Try again.")
