d9c84f155dafc5fe63429dabb1018a55870201b8
Ryan Gupta <ryangupta680@gmail.com>
2025-12-21 15:04:11 +0530

    Add helpers module with utility functions

--- functions/helpers.py
+++ functions/helpers.py
@@ -17,3 +17,16 @@ def percentage(part, whole):
     if whole == 0:
         return None
     return (part / whole) * 100
+
+def celsius_to_fahrenheit(c):
+    """Converts Celsius to Fahrenheit."""
+    return (c * 9 / 5) + 32
+
+def is_prime(n):
+    """Returns True if number is prime, else False."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
--- test.py
+++ test.py
@@ -1,35 +1,6 @@
-def introduce(name, age, country="India"):
-    return f"My name is {name}, I am {age} years old from {country}."
+from functions.helpers import *
 
-print(introduce("Ryan", 15))
-print(introduce(name="Ryan", age=15, country="Germany"))
-
-
-def total_sum(*numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    return total
-print(total_sum(1, 2, 3, 4, 10))
-
-def describe_person(**info):
-    for key, value in info.items():
-        print(f"{key}: {value}")
-describe_person(name="Ryan", hobby="Astronomy", mood="Curious")
-
-def bigger(a, b):
-    return a if a > b else b
-print (bigger(4,5))
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-print(is_prime(49))
-
-
-from projects.student_grade_calculator import*
-print(student_grade_calculator(4,90,90,99,99))
+print(is_even(10))
+print(celsius_to_fahrenheit(0))
+print(simple_interest(1000, 5, 2))
+print(is_prime(29))
